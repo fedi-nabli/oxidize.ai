c01ac7236c5a776b5f5f07eeeca2cccdbaa30892
fedi-nabli <fedinabli@gmail.com>
2024-09-21 21:20:17 +0100

    Started implemented Vector struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,48 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod math;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    use math::vector::Vector;
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_vector_add() {
+        let v1 = Vector { data: vec![1.2, 1.3, 1.5] };
+        let v2 = Vector { data: vec![1.8, 1.7, 1.5] };
+        let result = v1 + v2;
+        assert_eq!(result.data, vec![3.0, 3.0, 3.0]);
+    }
+
+    #[test]
+    fn test_vector_sub() {
+        let v1: Vector<i32> = Vector { data: vec![1, 2] };
+        let v2: Vector<i32> = Vector { data: vec![1, 1] };
+        let result = v1 - v2;
+        assert_eq!(result.data, vec![0, 1]);
+    }
+
+    #[test]
+    fn test_vector_mul() {
+        let v1 = Vector { data: vec![1.0] };
+        let v2 = Vector { data: vec![2.0] };
+        let result = v1 * v2;
+        assert_eq!(result.data, vec![2.0]);
+    }
+
+    #[test]
+    fn test_vector_div() {
+        let v1 = Vector { data: vec![1.0] };
+        let v2 = Vector { data: vec![2.0] };
+        let result = v1 / v2;
+        assert_eq!(result.data, vec![0.5]);
+    }
+
+    #[test]
+    fn test_vector_empty() {
+        let mut v: Vector<i32> = Vector { data: vec![] };
+        assert_eq!(v.is_empty(), true);
+        v.data = vec![1];
+        assert_eq!(v.is_empty(), false);
     }
 }
---
+++ src/math/vector.rs
@@ -0,0 +1,135 @@
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
+
+pub struct Vector<T = f64> {
+  pub data: Vec<T>
+}
+
+impl<T> Vector<T> {
+  pub fn is_empty(&self) -> bool {
+    self.data.is_empty()
+  }
+}
+
+impl<T> Add for Vector<T>
+where
+  T: Add<Output = T> + Copy,
+{
+  type Output = Self;
+
+  fn add(self, rhs: Self) -> Self::Output {
+    let data: Vec<T> = self
+      .data
+      .iter()
+      .zip(rhs.data.iter())
+      .map(|(a, b)| *a + *b)
+      .collect();
+    
+    Self {
+      data
+    }
+  }
+}
+
+impl<T> AddAssign for Vector<T>
+where
+  T: AddAssign + Copy
+{
+  fn add_assign(&mut self, rhs: Self) {
+    for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
+      *a += *b;
+    }
+  }
+}
+
+impl<T> Sub for Vector<T>
+where
+  T: Sub<Output = T> + Copy,
+{
+  type Output = Self;
+
+  fn sub(self, rhs: Self) -> Self::Output {
+    let data: Vec<T> = self
+      .data
+      .iter()
+      .zip(rhs.data.iter())
+      .map(|(a, b)| *a - *b)
+      .collect();
+
+    Self {
+      data
+    }
+  }
+}
+
+impl<T> SubAssign for Vector<T>
+where
+  T: SubAssign + Copy
+{
+  fn sub_assign(&mut self, rhs: Self) {
+    for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
+      *a -= *b;
+    }
+  }
+}
+
+impl<T> Mul for Vector<T>
+where
+  T: Mul<Output = T> + Copy
+{
+  type Output = Self;
+
+  fn mul(self, rhs: Self) -> Self::Output {
+    let data: Vec<T> = self
+      .data
+      .iter()
+      .zip(rhs.data.iter())
+      .map(|(a, b)| *a * *b)
+      .collect();
+
+    Self {
+      data
+    }
+  }
+}
+
+impl<T> MulAssign for Vector<T>
+where
+  T: MulAssign + Copy
+{
+  fn mul_assign(&mut self, rhs: Self) {
+    for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
+      *a *= *b;
+    }
+  }
+}
+
+impl<T> Div for Vector<T>
+where
+  T: Div<Output = T> + Copy
+{
+  type Output = Self;
+
+  fn div(self, rhs: Self) -> Self::Output {
+    let data: Vec<T> = self
+      .data
+      .iter()
+      .zip(rhs.data.iter())
+      .map(|(a, b)| *a / *b)
+      .collect();
+
+    Self {
+      data
+    }
+  }
+}
+
+impl<T> DivAssign for Vector<T>
+where
+  T: DivAssign + Copy
+{
+  fn div_assign(&mut self, rhs: Self) {
+    for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
+      *a /= *b;
+    }
+  }
+}
