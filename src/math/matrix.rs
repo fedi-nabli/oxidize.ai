5a2b57a68f8366f3fa35b0739c24b08849a77e65
fedi-nabli <fedinabli@gmail.com>
2024-09-22 15:52:08 +0100

    Implemented indexing for Matrix

--- src/math/matrix.rs
+++ src/math/matrix.rs
@@ -1,4 +1,5 @@
-use std::{fmt, ops::Index};
+use std::fmt;
+use std::ops::{Index, IndexMut};
 
 #[derive(Clone, Debug)]
 pub struct Matrix<T = f64> {
@@ -36,5 +37,31 @@ impl<T> Matrix<T> {
       cols,
       data: vec![T::from(1); rows * cols]
     }
+  }
+}
+
+impl<T> Index<(usize, usize)> for Matrix<T> {
+  type Output = T;
+
+  fn index(&self, (row, col): (usize, usize)) -> &Self::Output {
+    &self.data[row * self.cols + col]
+  }
+}
+
+impl<T> IndexMut<(usize, usize)> for Matrix<T> {
+  fn index_mut(&mut self, (row, col): (usize, usize)) -> &mut Self::Output {
+    &mut self.data[row * self.cols + col]
+  }
+}
+
+impl<T: fmt::Display> fmt::Display for Matrix<T> {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    for i in 0..self.rows {
+      for j in 0..self.cols {
+        write!(f, "{} ", self[(i, j)])?;
+      }
+      writeln!(f)?;
+    }
+    Ok(())
   }
 }
