1f4eb24411f358bccffe128c00e4c7e51a10be3c
fedi-nabli <fedinabli@gmail.com>
2024-12-05 21:26:09 +0100

    Implemented operatir overloading for Vector struct

--- src/math/vector.rs
+++ src/math/vector.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
 
 #[derive(Clone, PartialEq)]
 pub struct Vector<T = f64> {
@@ -145,6 +145,138 @@ impl<T> Vector<T> {
   {
     Vector {
       data: self.data.iter().zip(other.data.iter()).map(|(a, b)| f(a, b)).collect()
+    }
+  }
+}
+
+impl<T> FromIterator<T> for Vector<T> {
+  fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+    Vector {
+      data: iter.into_iter().collect()
+    }
+  }
+}
+
+impl<T> Add for Vector<T>
+where
+  T: Add<Output = T> + Copy
+{
+  type Output = Self;
+
+  fn add(self, rhs: Self) -> Self::Output {
+    let data: Vec<_> = self
+      .data
+      .iter()
+      .zip(rhs.data.iter())
+      .map(|(a, b)| *a + *b)
+      .collect();
+
+    Vector {
+      data
+    }
+  }
+}
+
+impl<T> AddAssign for Vector<T>
+where
+  T: AddAssign + Copy
+{
+  fn add_assign(&mut self, rhs: Self) {
+    for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
+      *a += *b
+    }
+  }
+}
+
+impl<T> Sub for Vector<T>
+where
+  T: Sub<Output = T> + Copy
+{
+  type Output = Self;
+
+  fn sub(self, rhs: Self) -> Self::Output {
+    let data: Vec<_> = self
+      .data
+      .iter()
+      .zip(rhs.data.iter())
+      .map(|(a, b)| *a - *b)
+      .collect();
+
+    Vector {
+      data
+    }
+  }
+}
+
+impl<T> SubAssign for Vector<T>
+where
+  T: SubAssign + Copy
+{
+  fn sub_assign(&mut self, rhs: Self) {
+    for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
+      *a -= *b
+    }
+  }
+}
+
+impl<T> Mul for Vector<T>
+where
+  T: Mul<Output = T> + Copy
+{
+  type Output = Self;
+
+  fn mul(self, rhs: Self) -> Self::Output {
+    let data: Vec<_> = self
+      .data
+      .iter()
+      .zip(rhs.data.iter())
+      .map(|(a, b)| *a * *b)
+      .collect();
+
+    Vector {
+      data
+    }
+  }
+}
+
+impl<T> MulAssign for Vector<T>
+where
+  T: MulAssign + Copy
+{
+  fn mul_assign(&mut self, rhs: Self) {
+    for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
+      *a *= *b
+    }
+  }
+}
+
+impl<T> Div for Vector<T>
+where
+  T: Div<Output = T> + Copy
+{
+  type Output = Self;
+
+  fn div(self, rhs: Self) -> Self::Output {
+    let data: Vec<_> = self
+      .data
+      .iter()
+      .zip(rhs.data.iter())
+      .map(|(a, b)| *a / *b)
+      .collect();
+
+    Vector {
+      data
+    }
+  }
+}
+
+impl<T> DivAssign for Vector<T>
+where
+  T: DivAssign + Copy
+{
+  fn div_assign(&mut self, rhs: Self) {
+    for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
+      *a /= *b
     }
   }
 }
